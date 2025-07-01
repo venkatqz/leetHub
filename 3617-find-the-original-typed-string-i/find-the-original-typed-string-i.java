db47ff105c63ab2c28bf74ab9ae69233e72a742e
Venkatesan <venkatqz@gmail.com>
2025-07-01 21:25:52 +0530

    Time: 1 ms (87.19%) | Memory: 42.3 MB (26.45%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.java
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.java
@@ -3,18 +3,16 @@ class Solution {
         //string itself + repeating letters count -1 or letters repeated. so hashmap to check
 
         //1+
-        int possible=1; 
-        HashMap<Character,Boolean> charoccur=new HashMap<Character,Boolean>();
-
-        charoccur.put(word.charAt(0),true);
-        for(int i=1;i<word.length();i++){
-            if(word.charAt(i)==word.charAt(i-1) && charoccur.containsKey(word.charAt(i))){
-                possible++;
+        int ans = 0;
+        char prevChar = word.charAt(0);
+        for (int i = 1; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (c == prevChar) {
+                ans++;
+            } else {
+                prevChar = c;
             }
-            else{charoccur.put(word.charAt(i),true);}
-
-
         }
-        return possible;
+        return ans + 1;
     }
 }
